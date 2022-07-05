6e87eb180c2f436ef0f75160b04cbbc36683f225
webagent-dev <webagent24@gmail.com>
2022-07-05 02:23:56 +0100

    add webpack config and badel config files

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        assetModuleFilename: "public/[hash]/[text][query]"
+    },
+    target: 'web',
+    devServer: {
+        port: 3000,
+        static: ["./public"],
+        open: true,
+        hot: true,
+        liveReload: true
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json"],
+    },
+    module: {
+        rules: [
+            {
+    test: /\.(js|jsx|ts)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', "@babel/preset-react"]
+                    }
+                }
+}
+        ]
+    },
+    plugins: [
+
+    ]
+}
